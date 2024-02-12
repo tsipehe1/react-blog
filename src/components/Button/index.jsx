cd75bc7c8d5f1fdee4d7e17881b18b059175400e
Helena Pereira <pereira-helena@outlook.com>
2024-02-12 18:36:29 +0000

    Remove defaultProps unused

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,7 @@
 import './styles.css';
 import P from 'prop-types';
 
-export const Button = ({ text, onClick, disabled = false }) => (
+export const Button = ({ text, onClick, disabled }) => (
   <button className="button" onClick={onClick} disabled={disabled}>
     {text}
   </button>
